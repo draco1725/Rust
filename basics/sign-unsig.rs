da39c92bc737b40ebcabe48042d2e3433a009027
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-28 18:08:25 +0530

    Create sign-unsig.rs

---
+++ basics/sign-unsig.rs
@@ -0,0 +1,23 @@
+//Signed = holds + and - values
+//Unsigned = holds only + values
+
+//Range
+
+//Signed:
+i8 = -128 to +127
+i16 = -32768 to + 32767
+
+Formula : -2^(n-1) to -2^(n-1)  //here "n" is 8  anythhing which you want it can be 16, 32, 128 any size
+
+
+//Unigned:
+u8 = 0 to 255
+u16 = 0 to 65535
+
+Formula: 0 to 2^(n)-1
+
+
+//Float : they holds decimal value 
+
+f32
+f64
