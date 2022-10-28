e280d1b18bdbb0823d3d05019d1db41e1c94ed60
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-29 01:20:09 +0530

    Update Sample.rs

--- basics/Sample.rs
+++ basics/Sample.rs
@@ -9,8 +9,7 @@ fn main() {
     println!("Welcome to the Game");
     let secret_num = rand::thread_rng().gen_range(1..101);
     //println!("Secret number is : {}",secret_num);
-
-
+    loop{
     println!("input your guess number");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
@@ -21,6 +20,11 @@ fn main() {
     match guess.cmp(&secret_num){
         Ordering:: Less => println!("Lesser bro"),
         Ordering:: Greater => println!("Too big bro num"),
-        Ordering:: Equal => println!("Dammnn bro you win"),
+        Ordering:: Equal => {
+            println!("Dammnn bro you win");
+            break;
+            }
+        }   
     }
+
 }
