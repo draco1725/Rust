b1ce0088b590a65560d521125695e0a4f5498d62
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-29 01:14:33 +0530

    Create Sample.rs

---
+++ basics/Sample.rs
@@ -0,0 +1,26 @@
+//Guessing Game
+
+
+use rand::{thread_rng, Rng};
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Welcome to the Game");
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    //println!("Secret number is : {}",secret_num);
+
+
+    println!("input your guess number");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read the line");
+    println!("Your Guess:{}",guess);
+    let guess:u32 = guess.trim().parse().expect("Type an integar"); //converting to integar
+
+    match guess.cmp(&secret_num){
+        Ordering:: Less => println!("Lesser bro"),
+        Ordering:: Greater => println!("Too big bro num"),
+        Ordering:: Equal => println!("Dammnn bro you win"),
+    }
+}
