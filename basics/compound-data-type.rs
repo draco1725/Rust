77cf4da973de5aeb09ec13870cc5cb5552bf4e8d
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-28 23:55:32 +0530

    Create compound-data-type.rs

---
+++ basics/compound-data-type.rs
@@ -0,0 +1,13 @@
+//Array
+fn main() {
+  let mut a = [1,2,3,4];
+  arr[3] = 5;  //here we changing the values in array and here use "mut" 
+  println!("{}",a[3]);
+}
+
+
+//tupple
+fn main() {
+  let tup = (10,3.14,'a',true);
+  println!("{}",tup.2);
+}
