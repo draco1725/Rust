9b9930151a4ca614a73da732ab24e33dfda52332
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-28 16:50:51 +0530

    Create Memory-safety.rs

---
+++ basics/Memory-safety.rs
@@ -0,0 +1,22 @@
+//here lets see an example
+
+fn main() {
+  let num;
+  println!("{}",num);
+} //this code wont work and gives out error as it make sure about memory safety here
+
+
+//Lets try another example
+
+fn main() {
+  let num;
+  
+  if true{
+      num=2;
+}
+  else {
+      num=4;
+}
+
+  println!("{}",num);
+}
