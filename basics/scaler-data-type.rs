a0526ba67d3a2e63a303615a94c77fa21c07a05e
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-28 23:47:25 +0530

    Create scaler-data-type.rs

---
+++ basics/scaler-data-type.rs
@@ -0,0 +1,22 @@
+//integar
+fn main() {
+  let x:u8 = 10;
+  println!("X value is {}",x)
+}
+
+//float
+fn main() {
+  let y:f32 = 10.00;
+  println!("Y value is {}",y);
+}
+
+//char
+fn main(){
+  let a 's';
+  println!("A value is {}",a);
+}
+
+//bool
+fn main() {
+  let b = true;  // we can either define it "b:bool" and use it
+  println!("B value is {}", b);
