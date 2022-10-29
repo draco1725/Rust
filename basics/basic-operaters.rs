e60053e4c547933cda9cd1531fa5d38c64ecfced
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-29 21:00:33 +0530

    Create basic-operaters.rs

---
+++ basics/basic-operaters.rs
@@ -0,0 +1,47 @@
+// Function returns
+fn give me slice() -> &'static str {
+    return "pizza"
+}
+fn give me string() -> String {
+   String::from(*pizza")
+}
+fn give me int() -> u32 {
+   4
+}
+
+
+// Conditionals
+fn conditional() {
+    let number = 3;
+    if number < 5 {
+        println! ("True!")
+    } else if number -- 5 {
+       println1("Ok!")
+    } else {
+       println!("False!")
+    }
+}
+
+// Loops
+fn while loop() {
+     loop {
+        println!("Infinite loop!");
+     }
+}
+ fn for loop() {
+     for n in 1..40 {
+        println!("Hello $(), n);
+     }
+}
+fn loop as variable() {
+     let mut counter = 0;
+     let g - loop {
+         if counter
+                       3
+             break
+                 "donut"
+             };
+         }
+         counter += 1
+     };
+}
