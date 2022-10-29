8626422cef05da24c2674ac1d60db83c4693d273
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-29 22:35:21 +0530

    Update compound-data-type.rs

--- basics/compound-data-type.rs
+++ basics/compound-data-type.rs
@@ -11,3 +11,15 @@ fn main() {
   let tup = (10,3.14,'a',true);
   println!("{}",tup.2);
 }
+
+// Tuples
+fn tuples() {
+    let tup: (i32, i32, String) = (1, 2, String::from("Paul"));
+    println! ("{}", tup.0);
+    println! ("{}", tup.1);
+    println! ("{}", tup.2);
+    let mut tup: (i32, 132, String) = (1, 2, String::from("Paul"));
+    tup.2 = String::from("John");
+    println! ("{}", tup.0);
+    println!("{}", tup.1);
+    println! ("{}", tup.2);
